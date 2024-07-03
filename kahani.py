6188a0af99ae2709296eb2c4ee1f1830a3799408
Muskan Yadav <muskanyadavlko2003@gmail.com>
2024-07-03 19:04:23 +0530

    #5 enumarate, for else ,zip, while,

---
+++ kahani.py
@@ -0,0 +1,10 @@
+kahani =""
+while True:
+    data=input("enter a story=>")
+    if len(data)==0:
+        print("The End!")
+        break
+    kahani +=data +"\n"
+
+print("The real story:")
+print(kahani)
--- pattern.1.py
+++ pattern.1.py
@@ -1,9 +1,24 @@
 from turtle import *
 
-# square
-side=10
-for i in range(side):
-    fd(120)
-    lt(360/side)
+# octagon
 
+s=getscreen()
+t=Turtle()
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
+t.fd(100)
+t.lt(45)
 mainloop()
+
